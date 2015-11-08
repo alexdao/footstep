a1fbd8d9297cb53e96e6c322965d3028a7d33361
Alex Dao <azd2@duke.edu>
2015-11-07 20:05:50 -0500

    Finish boilerplate for main activity rv

--- app/src/main/java/com/calliemao/gasmeter/ui/MainActivity.java
+++ app/src/main/java/com/calliemao/gasmeter/ui/MainActivity.java
@@ -5,23 +5,52 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.calliemao.gasmeter.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<String> stats;
+
+    @Bind(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Bind(R.id.fab)
+    FloatingActionButton fab;
+
+    @Bind(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    LinearLayoutManager llm;
+    RVAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        initializeData();
+        initializeRecyclerView(findViewById(R.id.recycler_view));
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -59,5 +88,90 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initializeData(){
+        stats = new ArrayList<>();
+    }
+
+    private void initializeRecyclerView(View view) {
+        adapter = new RVAdapter(stats, view);
+        llm = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(llm);
+        recyclerView.setAdapter(adapter);
+
+        RecyclerView.ItemDecoration itemDecoration = new
+                DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST);
+        recyclerView.addItemDecoration(itemDecoration);
+    }
+
+    public static class RVAdapter extends RecyclerView.Adapter<RVAdapter.MainStatViewHolder> {
+
+        List<String> stats;
+        View view;
+
+        RVAdapter(List<String> stats, View view) {
+            this.stats = stats;
+            this.view = view;
+        }
+
+        public void clear() {
+            stats.clear();
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public int getItemCount() {
+            return stats.size();
+        }
+
+        @Override
+        public MainStatViewHolder onCreateViewHolder(final ViewGroup viewGroup, int i) {
+            View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.recyclerview_item_stats, viewGroup, false);
+            MainStatViewHolder pvh = new MainStatViewHolder(v, new MainStatViewHolder.ISunlightDataViewHolderClicks() {
+                public void onClickItem(View caller) {
+                }
+            });
+            return pvh;
+        }
+
+        @Override
+        public void onBindViewHolder(MainStatViewHolder mainStatViewHolder, int i) {
+            mainStatViewHolder.rightTitle.setText(stats.get(i));
+            mainStatViewHolder.mainDescription.setText(stats.get(i));
+        }
+
+        @Override
+        public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+            super.onAttachedToRecyclerView(recyclerView);
+        }
+
+        public static class MainStatViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+            RelativeLayout relativeLayout;
+            ImageView statImage;
+            TextView rightTitle;
+            TextView mainDescription;
+            public ISunlightDataViewHolderClicks mListener;
+
+            MainStatViewHolder(View itemView, ISunlightDataViewHolderClicks listener) {
+                super(itemView);
+                mListener = listener;
+                relativeLayout = (RelativeLayout) itemView.findViewById(R.id.relative_layout);
+                relativeLayout.setOnClickListener(this);
+                statImage = ButterKnife.findById(itemView, R.id.stat_photo);
+                rightTitle = ButterKnife.findById(itemView, R.id.mainTitle);
+                mainDescription = ButterKnife.findById(itemView, R.id.mainDescription);
+            }
+
+            @Override
+            public void onClick(View v) {
+                mListener.onClickItem(v);
+            }
+
+            public interface ISunlightDataViewHolderClicks {
+                void onClickItem(View caller);
+            }
+        }
     }
 }
