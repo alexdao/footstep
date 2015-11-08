8d1f3df8618246bab3b5303475cbf5e746c15608
Callie Mao <cym4@duke.edu>
2015-11-07 21:28:32 -0500

    Beautified graph UI (color & animation)

--- app/src/main/java/com/calliemao/gasmeter/ui/WeeklyTimeline.java
+++ app/src/main/java/com/calliemao/gasmeter/ui/WeeklyTimeline.java
@@ -5,12 +5,18 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 import com.calliemao.gasmeter.R;
 import com.db.chart.model.Bar;
 import com.db.chart.model.BarSet;
 import com.db.chart.view.BarChartView;
+import com.db.chart.view.animation.Animation;
+import com.db.chart.view.animation.easing.BaseEasingMethod;
+import com.db.chart.view.animation.easing.CircEase;
+import com.db.chart.view.animation.easing.ExpoEase;
+import com.db.chart.view.animation.easing.LinearEase;
 
 public class WeeklyTimeline extends AppCompatActivity {
 
@@ -38,8 +44,15 @@ public class WeeklyTimeline extends AppCompatActivity {
         String[] testLabels = {"Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"};
         float[] testData = {20, 14, 17, 18, 22, 8, 6};
         BarSet test = new BarSet(testLabels, testData);
-
+        test.setColor(-10);
         barChartView.addData(test);
-        barChartView.show();
+
+        Animation animation = new Animation(1000);
+        //animation.setAlpha(0.5);
+        animation.setEasing(new LinearEase()); //ElasticEase
+
+//        barChartView.setAxisBorderValues(0, 100, 5);
+        barChartView.show(animation);
+
     }
 }
